450729f3c2774fe1e96605b4918b22ac9850e39a
Emilio <16996940+milongo@users.noreply.github.com>
2025-01-13 22:50:22 +0000

    refactor: safety

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,24 @@ fn main() {
         input.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
-
         // Wait for user input
         stdin.read_line(&mut input).unwrap();
         let input = input.trim();
+        if input.is_empty() {
+            continue;
+        }
+
         let mut command_parts = input.split_whitespace();
         let command = command_parts.next().unwrap();
         match command {
-            "exit" => exit(command_parts.next().unwrap().parse().unwrap()),
+            "exit" => {
+                if let Some(exit_code) = command_parts.next() {
+                    match exit_code.parse::<i32>() {
+                        Ok(code) => exit(code),
+                        Err(_) => println!("exit: invalid exit code"),
+                    }
+                }
+            },
             "echo" => println!("{}", command_parts.collect::<Vec<&str>>().join(" ")),
             "type" => {
                 if let Some(next_command) = command_parts.next() {
