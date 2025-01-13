7147a42433b811a46b103570f1b346b3d55ee55d
Emilio <16996940+milongo@users.noreply.github.com>
2025-01-13 22:15:28 +0000

    feat: type command

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,17 @@ fn main() {
         stdin.read_line(&mut input).unwrap();
         let input = input.trim();
         let mut command_parts = input.split_whitespace();
-
+        
         match command_parts.next().unwrap() {
             "exit" => exit(command_parts.next().unwrap().parse().unwrap()),
             "echo" => println!("{}", command_parts.collect::<Vec<&str>>().join(" ")),
-            _ => println!("{}: command not found", input),  
+            "type" => match command_parts.next().unwrap() {
+                "echo" => println!("echo is a shell builtin"),
+                "exit" => println!("exit is a shell builtin"),
+                "type" => println!("type is a shell builtin"),
+                _ => println!("{}: not found", input)
+            },
+            _ => println!("{}: command not found", input),
         }
     }
 }
