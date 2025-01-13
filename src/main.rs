aa61cd68358770565577587b4436163ad3396b9b
Emilio <16996940+milongo@users.noreply.github.com>
2025-01-13 19:27:59 +0000

    refactor: dry

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,20 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-
+    let stdin = io::stdin();
+    let mut input = String::new();
     loop {
+        input.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
 
         // Wait for user input
-        let stdin = io::stdin();
-        let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        if input.trim() == "exit 0" {
-            break;
-        }
-        else {
-            println!("{}: command not found", input.trim());
+        let input = input.trim();
+        match input {
+            "exit" => break,
+            _ => println!("{}: not found", input),
+            
         }
     }
 }
