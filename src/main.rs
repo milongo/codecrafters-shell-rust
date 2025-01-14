33c3d73515469b9275208918e74aefa2ce8b2d7a
Emilio <16996940+milongo@users.noreply.github.com>
2025-01-14 16:14:39 +0000

    feat: use Command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process::exit;
+use std::process::{exit, Command};
 use std::{env, path};
 
 fn main() {
@@ -35,59 +35,10 @@ fn main() {
                     exit(0); // Default exit code is 0
                 }
             },
-            "echo" => println!("{}", args.join(" ")),
-            "type" => {
-                if let Some(next_command) = args.get(0) {
-                    match *next_command {
-                        "echo" => println!("echo is a shell builtin"),
-                        "exit" => println!("exit is a shell builtin"),
-                        "type" => println!("type is a shell builtin"),
-                        _ => {
-                            let path = env::var("PATH").unwrap_or_else(|_| String::new());
-                            let paths = path.split(":");
-                            let mut found = false;
-
-                            for path in paths {
-                                let full_path = path::Path::new(path).join(next_command);
-                                if full_path.exists() {
-                                    println!("{} is {}", next_command, full_path.display());
-                                    found = true;
-                                    break;
-                                }
-                            }
-
-                            if !found {
-                                println!("{}: not found", next_command);
-                            }
-                        },
-                    }
-                } else {
-                    println!("type: missing argument");
-                }
-            },
             _ => {
-                let path = env::var("PATH").unwrap_or_else(|_| String::new());
-                let paths = path.split(":");
-                let mut found = false;
-
-                for path in paths {
-                    let full_path = path::Path::new(path).join(command);
-                    if full_path.exists() {
-                        println!("Program was passed {} args (including program name).",
-                            command_parts.len());
-
-                        println!("Arg #0 (program name): {}", command);
-                        for (i, arg) in args.iter().enumerate() {
-                            println!("Arg #{}: {}", i + 1, arg);
-                        }
-                        found = true;
-                        break;
-                    }
-                }
-
-                if !found {
-                    println!("{}: command not found", command);
-                }
+                let output = Command::new(command)
+                    .args(args)
+                    .output();
             },
         }
     }
