e4a9991891120201fb360f5b06e602f3ee9c78ac
Emilio <219734607+coactive-emilio@users.noreply.github.com>
2026-01-08 20:07:25 +0000

    redoing the thing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
-    // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+fn handle_command(input: &str) {
+    let mut input_parts = input.split_ascii_whitespace();
+    let command = input_parts.next();
+    let mut args = input_parts;
 
-    // Wait for user input
+    match command {
+        Some("exit") => std::process::exit(0),
+        Some("echo") => println!("{}", args.collect::<Vec<&str>>().join(" ")),
+        Some("type") => handle_type(args.next()),
+        _ => println!("{}: command not found", input),
+    }
+}
+
+fn handle_type(command: Option<&str>) {
+    match command {
+        Some("echo") => println!("echo is a shell builtin"),
+        Some("type") => println!("type is a shell builtin"),
+        Some("exit") => println!("exit is a shell builtin"),
+        Some(cmd) => println!("{}: not found", cmd),
+        _ => {}
+    }
+}
+
+fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    // Wait for user input
+    loop {
+        input.clear();
+
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        stdin.read_line(&mut input).unwrap();
+
+        let input = input.trim();
+
+        handle_command(input);
+    }
 }
